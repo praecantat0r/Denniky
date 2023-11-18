c82c414945f1ea50f9104bbad523423fc0386091
praecantat0r <ashianashi@gmail.com>
2023-11-18 19:32:39 +0100

    a

---
+++ src/Components/Post_preview.js
@@ -0,0 +1,18 @@
+function Post(props) {
+    return (
+    <div className="m-3 bg-white rounded-lg border-2 border-black overflow-hidden flex flex-row" style={{minHeight: "130px", maxHeight: "130px"}}> 
+        <div className="h-full w-1/3 bg-gray-200 border-r-2 border-black rounded-lg flex-none">
+            <div className="w-full h-full flex justify-center items-center"><p className="text-6xl">{props.Icon}</p></div>
+        </div>
+        <div className="flex flex-col w-full">
+        <div className="w-full">
+            <div className="w-full h-6 border-b-2 border-black"><p className="mx-1 font-dotgothic">{props.Title}</p></div>
+        </div>
+        <div className="w-full h-full">
+            <div className="w-full h-full"> <p className="text-gray-600 ml-2">{props.Description}</p></div>
+        </div>
+        </div>
+    </ div>
+    )
+}
+export default Post
--- src/Components/View_Posts.js
+++ src/Components/View_Posts.js
@@ -1,33 +1,52 @@
 import { useEffect, useState } from "react"
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { collection, getDoc, getDocs } from "firebase/firestore";
+import { QuerySnapshot, collection, getDoc, getDocs, onSnapshot } from "firebase/firestore";
 import { db } from "../firebase";
 import Footer from "./Footer"
+import Post from './Post_preview'
+import { get } from "firebase/database";
 function Create_Post() {
     const [userUid, setUserUid] = useState(null);
     const [userData, setUserData] = useState(null);
+    const [trips, setTrips] = useState([])
     const auth = getAuth();
   
     useEffect(() => {
       // Use Firebase Authentication to get the user's UID
-      onAuthStateChanged(auth, (user) => {
+      onAuthStateChanged(auth, async (user) => {
         if (user) {
           setUserUid(user.uid);
+          const getPosts = [];
+          const Posts = await getDocs(collection(db, user.uid))
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              getPosts.push({
+                ...doc.data(),
+                id: doc.id,
+              });
+            });
+          });
+          setTrips(getPosts);
+
         } else {
           setUserUid(null);
         }
       });
-    });
+    }, []);
+    
 
-    const getItems = async () => {
-        const querySnapshot = await getDocs(collection(db, userUid));
-        querySnapshot.forEach((doc) => {
-            console.log(doc.id, " => ", doc.data().Title);
-        });
-    };
-    useEffect(() => {
-        getItems();
-    })
+    // const getItems = async () => {
+    //     const list = [];
+    //     const querySnapshot = await getDocs(collection(db, userUid))
+    //         .then((querySnapshot) => {
+    //         const dataArray = querySnapshot.docs.map((doc) => ({
+    //             id: doc.id,
+    //             data: doc.data(),
+    //       }))
+    //       setTrips(dataArray);
+    //     });
+    // };
+
     return (
         <div>
 
@@ -35,68 +54,14 @@ function Create_Post() {
         <div>
             <div className="bg-[url('/Assets/main-small-bg.png')] flex h-screen items-center justify-center bg-cover 2xl:bg-[url('/Assets/background.png')] md:bg-[url('/Assets/md-screen-bg.png')]">
                 <div className="absolute h-[95vh] top-0 w-screen flex flex-col flex-nowrap overflow-auto">
-                    <div className="m-2 bg-white rounded-lg border-2 border-black flex flex-row" style={{height: "50px"}}>
-                        <div className="border-r-2 border-black h-full w-1/5 flex justify-center items-center text-center font-bold text-gray-600">1</div>
-                        <div className="border-r-2 border-black h-full w-1/5 flex justify-center items-center text-center font-bold text-gray-600">2</div>
-                        <div className="border-r-2 border-black h-full w-1/5 flex justify-center items-center text-center font-bold text-gray-600">3</div>
-                        <div className="border-r-2 border-black h-full w-1/5 flex justify-center items-center text-center font-bold text-gray-600">4</div>
-                        <div className=" w-1/5 flex justify-center items-center text-center font-bold text-gray-600 ">5</div>
-                    </div>
-                    <div className="m-3 bg-white rounded-lg border-2 border-black overflow-hidden flex flex-row" style={{height: "150px"}}> 
-                        <div className="h-full w-1/3 bg-gray-200 border-r-2 border-black rounded-lg flex-none">
-                            <div className="w-full h-full flex justify-center items-center"><img src="https://www.iconarchive.com/download/i141855/iconarchive/cute-animal/Cute-Penguin.1024.png" className=" h-20" alt="" /></div>
-                        </div>
-                        <div className="w-full">
-                            <div className="w-full h-6 border-b-2 border-black"><p className="mx-1 font-dotgothic">Skalka</p></div>
-                        </div>
-                    </ div>
-                    <div className="m-3 bg-white rounded-lg border-2 border-black overflow-hidden flex flex-row" style={{height: "150px"}}> 
-                        <div className="h-full w-1/3 bg-gray-200 border-r-2 border-black rounded-lg flex-none">
-                            <div className="w-full h-full flex justify-center items-center"><img src="https://www.iconarchive.com/download/i141855/iconarchive/cute-animal/Cute-Penguin.1024.png" className=" h-20" alt="" /></div>
-                        </div>
-                        <div className="w-full">
-                            <div className="w-full h-6 border-b-2 border-black"><p className="mx-1 font-dotgothic">Chopok</p></div>
-                        </div>
-                    </ div>
-                    <div className="m-3 bg-white rounded-lg border-2 border-black overflow-hidden flex flex-row" style={{height: "150px"}}> 
-                        <div className="h-full w-1/3 bg-gray-200 border-r-2 border-black rounded-lg flex-none">
-                            <div className="w-full h-full flex justify-center items-center"><img src="https://www.iconarchive.com/download/i141855/iconarchive/cute-animal/Cute-Penguin.1024.png" className=" h-20" alt="" /></div>
-                        </div>
-                        <div className="w-full">
-                            <div className="w-full h-6 border-b-2 border-black"><p className="mx-1 font-dotgothic">Suchy Vrch</p></div>
-                        </div>
-                    </ div>
-                    <div className="m-3 bg-white rounded-lg border-2 border-black overflow-hidden flex flex-row" style={{height: "150px"}}> 
-                        <div className="h-full w-1/3 bg-gray-200 border-r-2 border-black rounded-lg flex-none">
-                            <div className="w-full h-full flex justify-center items-center"><img src="https://www.iconarchive.com/download/i141855/iconarchive/cute-animal/Cute-Penguin.1024.png" className=" h-20" alt="" /></div>
-                        </div>
-                        <div className="w-full">
-                            <div className="w-full h-6 border-b-2 border-black"><p className="mx-1 font-dotgothic">Sachticka</p></div>
-                        </div>
-                    </ div>
-                    <div className="m-3 bg-white rounded-lg border-2 border-black overflow-hidden flex flex-row" style={{height: "150px"}}> 
-                        <div className="h-full w-1/3 bg-gray-200 border-r-2 border-black rounded-lg flex-none">
-                            <div className="w-full h-full flex justify-center items-center"><img src='https://www.iconarchive.com/download/i141855/iconarchive/cute-animal/Cute-Penguin.1024.png' className=" h-20" alt="" /></div>
-                        </div>
-                        <div className="w-full">
-                            <div className="w-full h-6 border-b-2 border-black"><p className="mx-1 font-dotgothic">Badin</p></div>
-                        </div>
-                    </ div>
-                    <div className="m-3 bg-white rounded-lg border-2 border-black overflow-hidden flex flex-row" style={{height: "150px"}}> 
-                        <div className="h-full w-1/3 bg-gray-200 border-r-2 border-black rounded-lg flex-none">
-                            <div className="w-full h-full flex justify-center items-center"><img src="https://www.iconarchive.com/download/i141855/iconarchive/cute-animal/Cute-Penguin.1024.png" className=" h-20" alt="" /></div>
-                        </div>
-                        <div className="w-full">
-                            <div className="w-full h-6 border-b-2 border-black"><p className="mx-1 font-dotgothic">Donovaly</p></div>
-                        </div>
-                    </ div>
-                    
+                  {trips.map(function(item, id){ 
+                    return <Post key={id} Title={item.Title} Description={item.Description} Icon={item.Icon}></Post>})}
                 </div>
             </div>
         </div>
     </body>
     <Footer></Footer>
     </div> 
-    )
+    );
 }
 export default Create_Post
